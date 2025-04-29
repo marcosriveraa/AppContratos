75bdbed8cc142e18aa7506dce0832edcf964d9e6
marcosriveraa <xmarcos.rivera@atalayamining.com>
2025-04-29 13:38:02 +0200

    AppContratos-V2

--- init.sh
+++ init.sh
@@ -8,12 +8,12 @@ mkdir -p /var/www/html/pendientes_firma /var/www/html/logs
 chown -R www-data:www-data /var/www/html/contratos_firmados /var/www/html/logs
 chown -R www-data:www-data /var/www/html/pendientes_firma /var/www/html/logs
 
-# Cambiar permisos (considera no usar 777 para entornos de producción, podrías usar 755 o 775 según sea necesario)
+# Cambiar permisos (considera no usar 777 para entornos de producciï¿½n, podrï¿½as usar 755 o 775 segï¿½n sea necesario)
 chmod -R 775 /var/www/html/contratos_firmados /var/www/html/logs
 chmod -R 775 /var/www/html/pendientes_firma /var/www/html/logs
 
-# Asegúrate de que el directorio de la aplicación web también tenga permisos adecuados
+# Asegï¿½rate de que el directorio de la aplicaciï¿½n web tambiï¿½n tenga permisos adecuados
 chmod -R 755 ./html
 
-# Iniciar Apache en primer plano para que el contenedor siga ejecutándose
+# Iniciar Apache en primer plano para que el contenedor siga ejecutï¿½ndose
 exec apache2-foreground
