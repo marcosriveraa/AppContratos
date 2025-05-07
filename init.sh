97d15992b669bf5e340a43c1d66a0974963488ef
marcosriveraa <xmarcos.rivera@atalayamining.com>
2025-05-07 08:29:40 +0200

    ContratosFinal

--- init.sh
+++ init.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # Crear las carpetas necesarias
-mkdir -p /var/www/html/contratos_firmados /var/www/html/logs
-mkdir -p /var/www/html/pendientes_firma /var/www/html/logs
+mkdir -p /var/www/html/contratos_firmados
+mkdir -p /var/www/html/pendientes_firma
+mkdir -p /var/www/html/logs
 
 # Cambiar la propiedad de las carpetas a 'www-data' (usuario por defecto de Apache)
-chown -R www-data:www-data /var/www/html/contratos_firmados /var/www/html/logs
-chown -R www-data:www-data /var/www/html/pendientes_firma /var/www/html/logs
+chown -R www-data:www-data /var/www/html
 
-# Cambiar permisos (considera no usar 777 para entornos de producci�n, podr�as usar 755 o 775 seg�n sea necesario)
-chmod -R 775 /var/www/html/contratos_firmados /var/www/html/logs
-chmod -R 775 /var/www/html/pendientes_firma /var/www/html/logs
+# Cambiar permisos (775 permite escritura por el grupo)
+chmod -R 775 /var/www/html
 
-# Aseg�rate de que el directorio de la aplicaci�n web tambi�n tenga permisos adecuados
-chmod -R 755 ./html
-
-# Iniciar Apache en primer plano para que el contenedor siga ejecut�ndose
+# Iniciar Apache en primer plano para que el contenedor siga ejecutándose
 exec apache2-foreground
